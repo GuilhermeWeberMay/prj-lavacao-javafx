b91bf9056fb48b5f94ead02e63bbe9212125d3e2
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-03-22 17:34:26 -0300

    Interface e classe para conectar o banco de dados

---
+++ prj-lavacao-javafx/src/main/java/br/edu/ifsc/fln/model/database/DataBase.java
@@ -0,0 +1,12 @@
+package br.edu.ifsc.fln.model.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public interface DataBase {
+    public Connection conectar();
+    public void desconectar(Connection conn);
+    public void commit(Connection connection);
+    public void rollback(Connection connection);
+}
