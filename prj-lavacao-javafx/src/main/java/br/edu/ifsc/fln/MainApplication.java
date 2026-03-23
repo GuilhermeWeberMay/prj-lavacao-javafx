499efde5a868067ad75cd4d0a9c8d6114800e823
Gustavo Rockembach <rockembach.dev@gmail.com>
2026-03-23 17:06:43 -0300

    feito sceneBuilder do cadastroCor

--- prj-lavacao-javafx/src/main/java/br/edu/ifsc/fln/MainApplication.java
+++ prj-lavacao-javafx/src/main/java/br/edu/ifsc/fln/MainApplication.java
@@ -11,8 +11,13 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+<<<<<<< Updated upstream
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Lavação dos Guri!");
+=======
+        Scene scene = new Scene(fxmlLoader.load(), 636, 630);
+        stage.setTitle("Hello!");
+>>>>>>> Stashed changes
         stage.setScene(scene);
         stage.show();
     }
---
+++ prj-lavacao-javafx/src/main/java/br/edu/ifsc/fln/controller/CadastroCorController.java
@@ -0,0 +1,30 @@
+package br.edu.ifsc.fln.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CadastroCorController {
+
+    @FXML
+    private Label labelDescCor;
+
+    @FXML
+    private Label labelIdCor;
+
+    @FXML
+    void buttonCreateCor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonDeleteCor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buttonUpdateCor(ActionEvent event) {
+
+    }
+
+}
