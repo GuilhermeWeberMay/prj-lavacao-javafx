b228cc427c78b8e7ea70c6220a0ad0ad3f9ef61c
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2026-05-04 20:53:19 -0300

    atualizado o banco para próxima etapa
    Atualizei o banco que é usado na aplicação e o banco de testes para ficarem na mesma "versão". Agora estão prontos para conseguirmos inserir clientes e veículos

--- prj-lavacao-javafx/src/main/resources/br/edu/ifsc/fln/bancoDeDados/db_lavacao.sql
+++ prj-lavacao-javafx/src/main/resources/br/edu/ifsc/fln/bancoDeDados/db_lavacao.sql
@@ -1,15 +1,55 @@
-#DROP DATABASE IF EXISTS db_lavacao;
-
 CREATE DATABASE db_lavacao;
 
 USE db_lavacao;
+
+CREATE TABLE IF NOT EXISTS cliente
+(
+    id            int          not null auto_increment,
+    nome          varchar(100) not null,
+    celular       varchar(11)  not null,
+    email         varchar(100) not null,
+    data_cadastro date         not null,
+    CONSTRAINT pk_cliente primary key (id)
+) Engine InnoDB;
+
+CREATE TABLE IF NOT EXISTS pessoa_fisica
+(
+    id_cliente      int         not null,
+    cpf             varchar(11) not null,
+    data_nascimento date        not null,
+    CONSTRAINT pk_pessoa_fisica primary key (id_cliente),
+    CONSTRAINT pk_pessoa_fisica_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) Engine InnoDB;
+
+INSERT INTO cliente (nome, celular, email, data_cadastro)
+VALUES ('Guilherme', '99999999999', 'guilherme.wm@aluno.ifsc.edu.br', '2026-05-04');
+INSERT INTO pessoa_fisica (id_cliente, cpf, data_nascimento)
+VALUES (1, '00000000000', '2006-10-24');
+
+CREATE TABLE IF NOT EXISTS pessoa_juridica
+(
+    id_cliente        int         not null,
+    cnpj              varchar(14) not null,
+    incricao_estadual varchar(15) not null,
+    CONSTRAINT pk_pessoa_juridica primary key (id_cliente),
+    CONSTRAINT pk_pessoa_juridica_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) Engine InnoDB;
+
+INSERT INTO cliente (nome, celular, email, data_cadastro)
+VALUES ('Rockembach Construções LTDA', '00000000000', 'rockcontrucoes@contato.com.br', '2026-05-04');
+INSERT INTO pessoa_juridica (id_cliente, cnpj, incricao_estadual)
+VALUES (2, '99999999999999', '123456');
 
 CREATE TABLE IF NOT EXISTS cor
 (
     id   INT         NOT NULL AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     CONSTRAINT pk_cor PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
 INSERT INTO cor (nome)
 VALUES ('Azul'),
@@ -22,7 +62,7 @@ CREATE TABLE IF NOT EXISTS marca
     id   INT         NOT NULL AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     CONSTRAINT pk_marca PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
 INSERT INTO marca (nome)
 VALUES ('Volkswagen'),
@@ -36,7 +76,7 @@ CREATE TABLE IF NOT EXISTS servico
     valor     DECIMAL(10, 2)                                        NOT NULL,
     categoria ENUM ('PEQUENO', 'MEDIO', 'GRANDE', 'MOTO', 'PADRAO') NOT NULL,
     CONSTRAINT pk_servico PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS configuracoes
 (
@@ -52,17 +92,16 @@ INSERT INTO servico (descricao, valor, categoria)
 VALUES ('lavagem simples', 50.00, 'GRANDE'),
        ('lavagem com cera', 70.00, 'MEDIO');
 
-CREATE TABLE if not exists modelo
+CREATE TABLE modelo
 (
     id        INT                                                   NOT NULL AUTO_INCREMENT,
     descricao VARCHAR(100)                                          NOT NULL,
     categoria ENUM ('PEQUENO', 'MEDIO', 'GRANDE', 'MOTO', 'PADRAO') NOT NULL,
     id_marca  INT                                                   NOT NULL,
     CONSTRAINT pk_modelo PRIMARY KEY (id),
-    CONSTRAINT fk_modelo_marca
-        FOREIGN KEY (id_marca)
-            REFERENCES marca (id)
-) ENGINE = InnoDB;
+    CONSTRAINT fk_modelo_marca FOREIGN KEY (id_marca)
+        REFERENCES marca (id)
+) ENGINE = INNODB;
 
 INSERT INTO modelo (descricao, categoria, id_marca)
 VALUES ('pulse', 'PADRAO', 3),
@@ -74,20 +113,34 @@ CREATE TABLE IF NOT EXISTS motor
     potencia         VARCHAR(100)                                                  NOT NULL,
     tipo_combustivel ENUM ('GASOLINA', 'ETANOL', 'FLEX', 'DIESEL', 'GNV', 'OUTRO') NOT NULL,
     CONSTRAINT pk_motor PRIMARY KEY (id_modelo),
-    CONSTRAINT fk_motor_modelo foreign key (id_modelo)
-        references modelo (id) on delete cascade on update cascade
-) ENGINE = InnoDB;
+    CONSTRAINT fk_motor_modelo FOREIGN KEY (id_modelo)
+        REFERENCES modelo (id)
+        ON DELETE CASCADE
+) ENGINE = INNODB;
 
 INSERT INTO motor (potencia, tipo_combustivel)
 VALUES ('1.0', 'FLEX'),
        ('1.6', 'DIESEL');
 
-/* UPDATE configuracoes set pontos_servico = 20 where id = 1; */
+CREATE TABLE IF NOT EXISTS veiculo
+(
+    id         INT          NOT NULL AUTO_INCREMENT,
+    placa      CHAR(7)      NOT NULL UNIQUE,
+    observacao VARCHAR(500) NOT NULL,
+    id_cor     INT          NOT NULL,
+    id_modelo  INT          NOT NULL,
+    id_cliente INT          NOT NULL,
+    CONSTRAINT pk_veiculo PRIMARY KEY (id),
+    CONSTRAINT fk_veiculo_cor FOREIGN KEY (id_cor)
+        REFERENCES cor (id),
+    CONSTRAINT fk_veiculo_modelo FOREIGN KEY (id_modelo)
+        REFERENCES modelo (id),
+    CONSTRAINT fk_veiculo_cliente FOREIGN KEY (id_cliente)
+        REFERENCES cliente (id)
+) ENGINE = INNODB;
 
--- Ver qual motor está sendo referenciado
-##
--- Veiculo vai ter chave extrangeira de cliente
-##
--- Motor vai ter id_modelo
--- Id de motor é o mesmo de modelo
-##
+INSERT INTO veiculo(placa, observacao, id_cor, id_modelo, id_cliente)
+VALUES ('SSF4B28', 'MEU CARRO', 1, 2, 1);
+
+INSERT INTO veiculo(placa, observacao, id_cor, id_modelo, id_cliente)
+VALUES ('GOL1234', 'GOL DA ALEMANHA', 2, 1, 2);
--- prj-lavacao-javafx/src/main/resources/br/edu/ifsc/fln/bancoDeDados/db_lavacao_teste.sql
+++ prj-lavacao-javafx/src/main/resources/br/edu/ifsc/fln/bancoDeDados/db_lavacao_teste.sql
@@ -4,12 +4,54 @@ CREATE DATABASE db_lavacao_teste;
 
 USE db_lavacao_teste;
 
+CREATE TABLE IF NOT EXISTS cliente
+(
+    id            int          not null auto_increment,
+    nome          varchar(100) not null,
+    celular       varchar(11)  not null,
+    email         varchar(100) not null,
+    data_cadastro date         not null,
+    CONSTRAINT pk_cliente primary key (id)
+) Engine InnoDB;
+
+CREATE TABLE IF NOT EXISTS pessoa_fisica
+(
+    id_cliente      int         not null,
+    cpf             varchar(11) not null,
+    data_nascimento date        not null,
+    CONSTRAINT pk_pessoa_fisica primary key (id_cliente),
+    CONSTRAINT pk_pessoa_fisica_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) Engine InnoDB;
+
+INSERT INTO cliente (nome, celular, email, data_cadastro)
+VALUES ('Guilherme', '99999999999', 'guilherme.wm@aluno.ifsc.edu.br', '2026-05-04');
+INSERT INTO pessoa_fisica (id_cliente, cpf, data_nascimento)
+VALUES (1, '00000000000', '2006-10-24');
+
+CREATE TABLE IF NOT EXISTS pessoa_juridica
+(
+    id_cliente        int         not null,
+    cnpj              varchar(14) not null,
+    incricao_estadual varchar(15) not null,
+    CONSTRAINT pk_pessoa_juridica primary key (id_cliente),
+    CONSTRAINT pk_pessoa_juridica_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) Engine InnoDB;
+
+INSERT INTO cliente (nome, celular, email, data_cadastro)
+VALUES ('Rockembach Construções LTDA', '00000000000', 'rockcontrucoes@contato.com.br', '2026-05-04');
+INSERT INTO pessoa_juridica (id_cliente, cnpj, incricao_estadual)
+VALUES (2, '99999999999999', '123456');
+
 CREATE TABLE IF NOT EXISTS cor
 (
     id   INT         NOT NULL AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     CONSTRAINT pk_cor PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
 INSERT INTO cor (nome)
 VALUES ('Azul'),
@@ -22,7 +64,7 @@ CREATE TABLE IF NOT EXISTS marca
     id   INT         NOT NULL AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     CONSTRAINT pk_marca PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
 INSERT INTO marca (nome)
 VALUES ('Volkswagen'),
@@ -31,13 +73,14 @@ VALUES ('Volkswagen'),
 
 CREATE TABLE IF NOT EXISTS servico
 (
-    id        INT           NOT NULL AUTO_INCREMENT,
-    descricao VARCHAR(100)  NOT NULL,
-    valor     DECIMAL(5, 2) NOT NULL,
+    id        INT                                                   NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(100)                                          NOT NULL,
+    valor     DECIMAL(10, 2)                                        NOT NULL,
+    categoria ENUM ('PEQUENO', 'MEDIO', 'GRANDE', 'MOTO', 'PADRAO') NOT NULL,
     CONSTRAINT pk_servico PRIMARY KEY (id)
-) ENGINE = InnoDB;
+) ENGINE = INNODB;
 
-CREATE TABLE configuracoes
+CREATE TABLE IF NOT EXISTS configuracoes
 (
     id             INT AUTO_INCREMENT,
     pontos_servico INT NOT NULL,
@@ -47,41 +90,39 @@ CREATE TABLE configuracoes
 INSERT INTO configuracoes (pontos_servico)
 VALUES (10);
 
-INSERT INTO servico (descricao, valor)
-VALUES ('lavagem simples', 50.00),
-       ('lavagem com cera', 70.00);
+INSERT INTO servico (descricao, valor, categoria)
+VALUES ('lavagem simples', 50.00, 'GRANDE'),
+       ('lavagem com cera', 70.00, 'MEDIO');
 
-CREATE TABLE IF NOT EXISTS motor
-(
-    id               INT                                                           NOT NULL AUTO_INCREMENT,
-    potencia         VARCHAR(100)                                                  NOT NULL,
-    tipo_combustivel ENUM ('GASOLINA', 'ETANOL', 'FLEX', 'DIESEL', 'GNV', 'OUTRO') NOT NULL,
-    CONSTRAINT pk_motor PRIMARY KEY (id)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS modelo
+CREATE TABLE modelo
 (
     id        INT                                                   NOT NULL AUTO_INCREMENT,
     descricao VARCHAR(100)                                          NOT NULL,
     categoria ENUM ('PEQUENO', 'MEDIO', 'GRANDE', 'MOTO', 'PADRAO') NOT NULL,
     id_marca  INT                                                   NOT NULL,
-    id_motor  INT                                                   NOT NULL,
     CONSTRAINT pk_modelo PRIMARY KEY (id),
-    CONSTRAINT fk_modelo_marca
-        FOREIGN KEY (id_marca)
-            REFERENCES marca (id),
-    CONSTRAINT fk_modelo_motor
-        FOREIGN KEY (id_motor)
-            REFERENCES motor (id) ON DELETE CASCADE
-) ENGINE = InnoDB;
+    CONSTRAINT fk_modelo_marca FOREIGN KEY (id_marca)
+        REFERENCES marca (id)
+) ENGINE = INNODB;
+
+INSERT INTO modelo (descricao, categoria, id_marca)
+VALUES ('pulse', 'PADRAO', 3),
+       ('versa', 'MEDIO', 2);
+
+CREATE TABLE IF NOT EXISTS motor
+(
+    id_modelo        INT                                                           NOT NULL AUTO_INCREMENT,
+    potencia         VARCHAR(100)                                                  NOT NULL,
+    tipo_combustivel ENUM ('GASOLINA', 'ETANOL', 'FLEX', 'DIESEL', 'GNV', 'OUTRO') NOT NULL,
+    CONSTRAINT pk_motor PRIMARY KEY (id_modelo),
+    CONSTRAINT fk_motor_modelo FOREIGN KEY (id_modelo)
+        REFERENCES modelo (id)
+        ON DELETE CASCADE
+) ENGINE = INNODB;
 
 INSERT INTO motor (potencia, tipo_combustivel)
 VALUES ('1.0', 'FLEX'),
        ('1.6', 'DIESEL');
-
-INSERT INTO modelo (descricao, categoria, id_marca, id_motor)
-VALUES ('pulse', 'PADRAO', 3, 1),
-       ('versa', 'MEDIO', 2, 2);
 
 CREATE TABLE IF NOT EXISTS veiculo
 (
@@ -90,40 +131,17 @@ CREATE TABLE IF NOT EXISTS veiculo
     observacao VARCHAR(500) NOT NULL,
     id_cor     INT          NOT NULL,
     id_modelo  INT          NOT NULL,
+    id_cliente INT          NOT NULL,
     CONSTRAINT pk_veiculo PRIMARY KEY (id),
-    CONSTRAINT fk_veiculo_cor
-        FOREIGN KEY (id_cor)
-            REFERENCES cor (id),
-    CONSTRAINT fk_veiculo_modelo
-        FOREIGN KEY (id_modelo)
-            REFERENCES modelo (id)
-) ENGINE = InnoDB;
+    CONSTRAINT fk_veiculo_cor FOREIGN KEY (id_cor)
+        REFERENCES cor (id),
+    CONSTRAINT fk_veiculo_modelo FOREIGN KEY (id_modelo)
+        REFERENCES modelo (id),
+    CONSTRAINT fk_veiculo_cliente FOREIGN KEY (id_cliente)
+        REFERENCES cliente (id)
+) ENGINE = INNODB;
 
-INSERT INTO veiculo(placa, observacao, id_cor, id_modelo)
-VALUES ('SSF4B28', 'MEU CARRO', 1, 2);
+INSERT INTO veiculo(placa, observacao, id_cor, id_modelo, id_cliente)
+VALUES ('SSF4B28', 'MEU CARRO', 1, 2, 1);
 
 /* UPDATE configuracoes set pontos_servico = 20 where id = 1; */
-
-SELECT m.descricao, m.categoria, ma.nome, mo.potencia, mo.tipo_combustivel
-FROM modelo m
-         INNER JOIN marca ma ON m.id_marca = ma.id
-         INNER JOIN motor mo ON m.id_motor = mo.id;
-
-SELECT v.placa,
-       v.observacao,
-       c.nome  AS cor,
-       m.descricao,
-       m.categoria,
-       ma.nome AS marca,
-       mo.potencia,
-       mo.tipo_combustivel
-FROM veiculo v
-         INNER JOIN cor c ON v.id_cor = c.id
-         INNER JOIN modelo m ON v.id_modelo = m.id
-         INNER JOIN marca ma ON m.id_marca = ma.id
-         INNER JOIN motor mo ON m.id_motor = mo.id;
-
-SELECT s.id, s.descricao, s.valor, c.pontos_servico
-FROM servico s
-         CROSS JOIN configuracoes c
-WHERE c.id = 1;
